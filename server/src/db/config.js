7163b8c708b4f5652bbd8ac43b99ee3f9fa6eaa6
LoisClinton <loisclinton@hotmail.co.uk>
2023-11-03 11:36:03 +0000

    added User tests and added the sequelize connection in config.js

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,15 @@
-const { Sequelize } = require("sequelize");
+const path = require("path");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const db = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+  logging: false,
+});
+
+module.exports = {
+  db,
+  Sequelize,
+  Model,
+  DataTypes,
+};
--- server/src/models/User.test.js
+++ server/src/models/User.test.js
@@ -1,26 +1,34 @@
-const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals')
-const { User } = require('.')
-const db = require('../db/config')
+const { describe, it, expect, beforeAll, afterAll } = require("@jest/globals");
+const { User } = require("./index.js");
+const { db } = require("../db/config");
 
 // define in global scope
-let user
+let user;
 
 // clear db and create new user before tests
 beforeAll(async () => {
-  await db.sync({ force: true })
-  user = await User.create({ username: 'gandalf' })
-})
+  await db.sync({ force: true });
+  user = await User.create({ username: "gandalf" });
+});
 
 // clear db after tests
-afterAll(async () => await db.sync({ force: true }))
+afterAll(async () => await db.sync({ force: true }));
 
-describe('User', () => {
-  it('has an id', async () => {
-    expect(user).toHaveProperty('id')
-  })
+describe("User", () => {
+  it("Is an instnce of User", async () => {
+    expect(user).toBeInstanceOf(User);
+  });
+
+  it("has a", async () => {
+    expect(user).toHaveProperty("id");
+  });
+
+  it("has the correct username", async () => {
+    expect(user.username).toBe("gandalf");
+  });
 
   /**
    * Create more tests
    * E.g. check that the username of the created user is actually gandalf
    */
-})
+});
